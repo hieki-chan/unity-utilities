e62e0e5a483a9249c172d28e900cc3dc2a6fa706
@hieki_chan <92980856+hieki-chan@users.noreply.github.com>
2024-08-22 22:29:15 +0700

    upload reflection extends

---
+++ Hieki.Utils/Extensions/ReflectionExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+using System.Reflection;
+
+namespace Hieki.Utils
+{
+    public static class ReflectionExtensions
+    {
+        public static void SetField<V>(this object target, V value, string fieldName = null)
+        {
+            FieldInfo info = target.GetType().GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance)
+                .Where(f => f.FieldType == typeof(V) && (string.IsNullOrEmpty(fieldName) ? true : f.Name == fieldName))
+                .FirstOrDefault();
+
+            info?.SetValue(target, value);
+        }
+
+        public static V GetField<V>(this object target, string fieldName = null)
+        {
+            FieldInfo info = target.GetType().GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance)
+                .Where(f => f.FieldType == typeof(V) && (string.IsNullOrEmpty(fieldName) ? true : f.Name == fieldName))
+                .FirstOrDefault();
+
+            if (info == null)
+                return default;
+
+            return (V)(info.GetValue(target));
+        }
+    }
+}
